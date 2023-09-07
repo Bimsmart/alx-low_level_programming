3dd42fbfb512bc204be66b2e4454ab201a18645d
Bimsmart <bolaewenla66@gmail.com>
2023-09-07 14:43:49 -0700

    using sizeof() to get the size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,14 @@ int main(void)
 	int i;
 	double d;
 	char c;
+	float f;
+	short s;
 	printf("The size of an integer is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
 	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
-	return(0);
+	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(i));
+        printf("The size of a short is: %lu.\n", (unsigned long)sizeof(s));
+        printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c)
+	return (0);
 }
 
