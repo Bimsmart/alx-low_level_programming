337e8b678536bea30cc196afc3a072a5f4210a98
Bimsmart <bolaewenla66@gmail.com>
2023-09-07 15:20:26 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,11 @@
 /*find the size of various types*/
 int main(void)
 {
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of short: %zu bytes\n", sizeof(short));	
+	printf("Size of a char: %zu bytes \n", sizeof(char));
+	printf("Size of an int: %zu bytes \n", sizeof(int));
+	printf("Size of a long int: %zu bytes \n", sizeof(long int);
+	printf("Size of a long long int: %zu bytes \n", sizeof(long long int));
+	printf("Size of a float: %zu bytes \n", sizeof(float));	
 	return (0);
 }
 
