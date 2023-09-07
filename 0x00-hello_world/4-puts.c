97edacd5274ac1d13345815a2a5c382695faea13
Bimsmart <bolaewenla66@gmail.com>
2023-09-07 16:37:40 -0700

    using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
-/*
- * main - Entry point
- * Description: 'Using puts instead of print'
- * 
- * Return: Always 0 (success)
+/**
+ * main - a main function
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*printf statement*/
+/**
+ * main - a main function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
