8b9bba12b21983707780875a18d63328c9dfada2
Bimsmart <bolaewenla66@gmail.com>
2023-09-07 16:12:35 -0700

    size of

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /*
  * main - Entry point
- * Description: Using puts instead of print
+ * Description: 'Using puts instead of print'
  * 
  * Return: Always 0 (success)
  */
+
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+
 /*find the size of various types*/
+
 int main(void)
 {
-	printf("Size of a char: %zu bytes \n", sizeof(char));
-	printf("Size of an int: %zu bytes \n", sizeof(int));
+	char c;
+	int i;
+	double d;
+	float f;
+
+	printf("Size of a char: %zu bytes \n", sizeof(c));
+	printf("Size of an int: %zu bytes \n", sizeof(i));
 	printf("Size of a long int: %zu bytes \n", sizeof(long int);
 	printf("Size of a long long int: %zu bytes \n", sizeof(long long int));
 	printf("Size of a float: %zu bytes \n", sizeof(float));	
