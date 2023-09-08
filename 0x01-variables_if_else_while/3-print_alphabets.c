a9d4ffd1d1edcbff484dfa7ece475c46cca50138
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 14:22:55 -0700

    print lowecase letters first followed by upper case letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	char alpha;
 	
-	for (alpha = 'a'; alpha < 'z' ; alpha++)
+	for (alpha = 'a'; alpha <= 'z' ; alpha++)
 	{
 		putchar(alpha);
 	}
 	
-	for (alpha = 'A'; alpha < 'Z'; alpha++)
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 	{
 		putchar(alpha);
 	}
