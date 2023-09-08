3e1eedcbc66f1317c6d5e393935a9230bf7b8f77
Bimsmart <bolaewenla66@gmail.com>
2023-09-07 17:15:11 -0700

    sizes of int, char, float. long int and long long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,15 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld bytes\n", sizeof(char));
-	printf("Size of a int: %ld bytes\n", sizeof(int));
-	printf("Size of a long int: %ld bytes\n", sizeof(long int));
-	printf("Size of a long long: %ld bytes\n", sizeof(long long));
-	printf("Size of a float: %ld bytes\n", sizeof(float);
+	char c;
+	int i;
+	long int l;
+	long long int m;
+	float f;
+
+	printf("Size of a char: %ld bytes\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %ld bytes\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %ld bytes\n", (unsigned long)sizeof(l));
+	printf("Size of a long long: %ld bytes\n", (unsigned long)sizeof(m));
+	printf("Size of a float: %ld bytes\n", (unsigned long)sizeof(f);
 }
