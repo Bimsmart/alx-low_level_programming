e8ab82a57aab5b48dfc89ce06d8b0e19908c02c2
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 12:45:21 -0700

    is the last digit greater than 5, less than 6 or equal to 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-1-last_digit.c * main - This is the Main function
+ * main - This is the Main function
  *
  * Return: Always 0 (Success)
  */
@@ -16,17 +16,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	if (n > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-	}
 	if (n == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	if (n < 6 && n != 0)
+	else if (n < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
 	return (0);
 }
