2b8b17d52a9f271679585d0aa580117ff62fa468
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 16:32:48 -0700

    all possible combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - THis is a Main function
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(d + '0');
+	if (d != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	return (0);
+}
