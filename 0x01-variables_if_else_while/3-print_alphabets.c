949e5644df3af62b8581537fbeb60f06759aa583
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 14:16:51 -0700

    print lowecase letters first followed by upper case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ * main - This is a main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha < 'z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	
+	for (alpha = 'A'; alpha < 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
