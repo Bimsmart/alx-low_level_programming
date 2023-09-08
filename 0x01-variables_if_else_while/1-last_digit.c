50fb32216fc2ab0a29f51f9156357b247dbfd575
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 09:36:23 -0700

    is the last digit greater than 5, less than 6 but not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ int main(void)
 	}
 	if (n == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	if (n < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, l);
+		printf("Last digit of %d is %d and is less than 6 and not  0\n", n, l);
 	}
 	return (0);
 }
