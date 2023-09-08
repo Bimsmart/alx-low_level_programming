a60216ffaac79ff6f1fe622439cc01a5cbf78de3
Bimsmart <bolaewenla66@gmail.com>
2023-09-07 17:37:42 -0700

    sizes of int, char, float. long int and long long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
- * main - a main function
- * return: Always 0 (Success)
+ * main - this is the entry point
+ * return: Always 0 (success)
  */
 int main(void)
 {
 	char c;
 	int i;
-	long int l;
-	long long int m;
+	long int d;
+	long long int e;
 	float f;
 
-	printf("Size of a char: %ld bytes\n", (unsigned long)sizeof(c));
-	printf("Size of a int: %ld bytes\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %ld bytes\n", (unsigned long)sizeof(l));
-	printf("Size of a long long: %ld bytes\n", (unsigned long)sizeof(m));
-	printf("Size of a float: %ld bytes\n", (unsigned long)sizeof(f);
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(d);
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(e);
+	printf("Size of a float: %ld: byte(s)\n", (unsigned long)sizeof(f);
+	return (0);
 }
