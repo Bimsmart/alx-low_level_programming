b10371b0d8851b620f3b7acf1531889dd858a76d
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 08:34:33 -0700

    is it positive or negative?

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - This is a Main function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
