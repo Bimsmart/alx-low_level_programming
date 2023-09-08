54977f9716125e9ecfeddd69fbefd12cf58cdef5
Bimsmart <bolaewenla66@gmail.com>
2023-09-08 10:14:49 -0700

    is the last digit greater than 5, less than 6 but not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
- * main - This is a Main function
+ * main - This is the Main function
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
