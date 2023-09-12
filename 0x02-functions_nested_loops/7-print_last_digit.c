2c557ba316d4a86ff5bdedfa0cd82eb13bc37112
Bimsmart <bolaewenla66@gmail.com>
2023-09-12 15:15:21 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - this function Prints the last digit of a number
+ *@n: The number whose last digit is to be calculated
+ * Return: 0
+ *
+ */
+int print_last_digit(int n)
+{
+	int l = n % 10;
+
+	return (l);
+}
