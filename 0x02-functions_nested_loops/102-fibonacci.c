63063702f0080e3846b3a5f2b3337f56e0a5edd2
Bimsmart <bolaewenla66@gmail.com>
2023-09-12 19:10:03 -0700

    102 - fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -19,17 +19,17 @@ int main(void)
 
 	else if (i == 1)
 	{
-		printf(",%ld", k);
+		printf(", %ld", k);
 	}
 	else
 	{
 		k += j;
 		j = k - j;
-		printf(",%ld", k);
+		printf(", %ld", k);
 	}
 		i++;
 	}
-		printf(' ');
+
 		printf("\n");
 		return (0);
 }
