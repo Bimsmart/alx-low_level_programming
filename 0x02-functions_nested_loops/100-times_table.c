80c6eed47faec20e606411009e4647008a859eee
Bimsmart <bolaewenla66@gmail.com>
2023-09-13 04:49:47 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k = j * i;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -14,27 +14,25 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
-			}
-			else if (k < 10 && j != 0)
+			} else if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}
-			else if (k >= 10 && k < 100)
+			} else if (k >= 10 && k < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-			}
-			else if (k >= 100)
+			} else if (k >= 100)
 			{
 				_putchar(',');
 				_putchar(' ');
