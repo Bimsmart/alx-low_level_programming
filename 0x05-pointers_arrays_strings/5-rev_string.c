ef459139f67c5a442d281b6d957acc076f9ad527
Bimsmart <bolaewenla66@gmail.com>
2023-09-18 13:45:51 -0700

    reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - this reverses a string
+ * @s: this is a string
+ * Return: void;
+ */
+void rev_string(char *s)
+{
+	int l;
+	int count = 0;
+
+	for (l = 0; s[l] != 0; l++)
+		count++;
+	for (l = 0; l < count / 2; l++)
+	{
+	char m = s[l];
+	s[l] = s[count - 1 - l];
+	s[count -1 - l] = m;
+	}
+}
