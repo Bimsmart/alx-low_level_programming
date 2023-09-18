f229168333a0a3890e380aa0b29b8dbebb4da472
Bimsmart <bolaewenla66@gmail.com>
2023-09-18 09:54:12 -0700

    printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - a funtion to print a string.
+ * @str: the wordvor sentence to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
