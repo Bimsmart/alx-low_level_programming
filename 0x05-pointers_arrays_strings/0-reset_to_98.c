240fb01573468980ef921657065b3d59c5b91c12
Bimsmart <bolaewenla66@gmail.com>
2023-09-18 03:51:00 -0700

    pointers - update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function updates the value of a pointer
+ * @n: this the integer to be updated
+ * return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
