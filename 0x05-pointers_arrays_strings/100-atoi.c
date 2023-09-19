3b3dee28327f0abbb39320c50fc15aa7f614b548
Bimsmart <bolaewenla66@gmail.com>
2023-09-19 13:37:14 -0700

    string conversion to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,35 +6,21 @@
  */
 int _atoi(char *s)
 {
-	int l, d, f, length, n, digit;
+	int sign = 1;
+	int result = 0;
+	int m = 0;
 
-	l = 0;
-	d = 0;
-	f = 0;
-	length = 0;
-	n = 0;
-	digit = 0
-	while (s[length] != '\0'){
-		len++;
-	while (l < length && f == 0)
-	{	
-		if (s[l] == '-')
-			++d;
-		if (s[l] >= '0' && s[l] <= '9')
-		{
-			digit = s[l] - '0';
-			if (d % 2)
-				digit = - digit;
-			n = n * 10 + digit;
-			f = 1;
-			}
-		if (s[l + 1] < '0' || s[l + 1] > '9')
-			break;
-			f = 0;
-		}
-			l++;
-		if (f == 0)
-			return (0);
+	if (s[0] == '-')
+	{	sign = -1;
+		m++;
 	}
-		return (n);
+	while (s[m] != '\0')
+	{
+		if (s[m] >= '0' && s[m] <= '9')
+		{
+			result = result * 10 + (s[m] - '0');
+		}
+		m++;
+	}
+	return (sign * result);
 }
