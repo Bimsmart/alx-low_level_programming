62ffddba1591c316336983f1e8ad158346070081
Bimsmart <bolaewenla66@gmail.com>
2023-09-20 09:36:03 -0700

    concatenating two strings without terminating the null character of the second if it contains n more bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - this function concatenates two string
+ * and the length must not exceed the length of the second 
+ * @desc: the destination
+ * @ the source string
+ * return: *desc
+ */
+ char *_strncat(char *dest, char *src, int n)
+{
+	int length = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[length + i] = src[i];
+	}
+	dest[length + i] = '\0';
+	return (dest);
+}
