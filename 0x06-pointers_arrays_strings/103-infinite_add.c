33783a2eab66ffc5dbe271d423a1df052a875ac8
Bimsmart <bolaewenla66@gmail.com>
2023-09-21 16:35:59 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,26 +1,28 @@
 #include "main.h"
 /**
- * rev_str(char *n)
- * @n: integer
- * Return 0
+ * rev_string - this reverses a string or array
+ * @n: integers
+ * Return: 0
  */
-void rev_str(char*n)
+void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
 	char temp;
+
 	while (*(n + i) != '\0')
 	{
 		i++;
 	}
 	i--;
-	for (i = 0;  j < i; j++, i--)
+	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
 }
+
 
 /**
  * infinite_add - this adds two numbers
@@ -32,8 +34,8 @@ void rev_str(char*n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int excess = 0; 
-	int i = 0, j= 0, d = 0;
+	int excess = 0;
+	int i = 0, j = 0, d = 0;
 	int v1 = 0, v2 = 0, temp_sum = 0;
 
 	while (*(n1 + i) != '\0')
@@ -43,11 +45,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*(n2 + j) != '\0')
 	{
 		j++;
-		i--;
-		j--;
 	}
+	i--;
+	j--;
+
 	if (j >= size_r || i >= size_r)
-	       return (0);
+		return (0);
 	while (j >= 0 || i >= 0 || excess == 1)
 	{
 		if (i < 0)
@@ -63,7 +66,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			excess = 1;
 		else
 			excess = 0;
-		if(d >= (size_r -1))
+		if (d >= (size_r - 1))
 			return (0);
 		*(r + d) = (temp_sum % 10) + '0';
 		d++;
@@ -71,8 +74,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		i--;
 	}
 	if (d == size_r)
-	return (0);
+		return (0);
 	*(r + d) = '\0';
-	rev_str(r);
+	rev_string(r);
 	return (r);
 }
