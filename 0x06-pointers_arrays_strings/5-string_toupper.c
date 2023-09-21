9243d5d227797bdbd46b3796cd94cad1e16f8c80
Bimsmart <bolaewenla66@gmail.com>
2023-09-20 17:44:49 -0700

    Strings:from lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,19 @@
-#include "main.h" 
+#include "main.h"
+#include <ctype.h>
 /**
- * string_toupper - a function that changes letters of 
+ * string_toupper - a function that changes letters from lowercase to uppercase
+ * @str: a string
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+		while (str[i] != '\0')
+		{
+			str[i] = toupper(str[i]);
+			i++;
+		}
+			return (str);
+}
+
