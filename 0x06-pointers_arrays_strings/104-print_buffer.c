e6b6b87b10158eef23b1af7ef9d865c197e0c343
Bimsmart <bolaewenla66@gmail.com>
2023-09-21 18:17:12 -0700

    print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - this function orints a buffer
+ * print_buffer - this function prints a buffer
  * @b: a pointer to a buffer
  * @size: size of the buffer
  * Return: void
@@ -22,27 +22,27 @@ void print_buffer(char *b, int size)
 	{
 		q = size - p < 10 ? size - p : 10;
 		printf("%08x: ", p);
-	for (r = 0; r < 10; r++)
-	{
-		if (r < q)
-			printf("%02x", *(b + p + r));
-		else
-			printf(" ");
-		if (r % 2)
+		for (r = 0; r < 10; r++)
 		{
-			printf(" ");
+			if (r < q)
+				printf("%02x", *(b + p + r));
+			else
+				printf("  ");
+			if (r % 2)
+			{
+				printf(" ");
+			}
 		}
-	}
-	for (r = 0; r < q; r++)
-	{
-		int c = *(b + p + r);
-	if (c < 32 || c > 132)
-	{
-		c = '.';
-	}
-		printf("%c", c);
-	}
-	printf("\n");
-	p += 10;
+		for (r = 0; r < q; r++)
+		{
+			int c = *(b + p + r);
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		p += 10;
 	}
 }
