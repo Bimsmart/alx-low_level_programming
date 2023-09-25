36bb7f272d02031d17ee02f979486d52e58e3d1a
Bimsmart <bolaewenla66@gmail.com>
2023-09-25 14:45:23 -0700

    this function locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - this function locates a character in a string
  * @s: the string
@@ -17,4 +18,4 @@ char *_strchr(char *s, char c)
 		}
 	}
 			return (NULL);
-}
+}	
