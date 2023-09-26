602a2b50058a4b6df1559024115c07eb56db2630
Bimsmart <bolaewenla66@gmail.com>
2023-09-25 17:01:29 -0700

    this function searches for any form of byte in a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - this function searches a string for any form of bytr
+ * @s: the string
+ * @accept: the byte to be matched
+ * Return: apointer to s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		char *a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+
+	}
+	return (NULL);
+}
