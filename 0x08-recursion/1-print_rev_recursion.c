935d912c8b9c9b347e45ddcc7dac5d8c877a9ab4
Bimsmart <bolaewenla66@gmail.com>
2023-09-28 11:46:20 -0700

    this function prints a string  in reverser0

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0' && *s != '\n')
 	{
-		_putchar('\n');
+		return;
 	}
 	else
 	{
-		_print_rev_recursion(++s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
