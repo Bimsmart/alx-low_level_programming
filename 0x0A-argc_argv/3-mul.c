4a97e61ea97a5702d27d08d8803ada49554cab43
Bimsmart <bolaewenla66@gmail.com>
2023-10-02 12:02:12 -0700

    this program multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * @argc: argument to count
+ * @argv: argument to a pointer to a string
+ * Return: 0 (Always)
+ */
+int main(int argc, char *argv[])
+{
+	int c, product = 1;
+
+	if (argc > 1)
+	{
+		for (c = 1; c < argc; c++)
+		{
+			product *= atoi(argv[c]);
+		}
+		printf("%d\n", product);
+	}
+	else
+	{
+		for (c = 0 && c >= 3; c < argc; c++)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	return (0);
+}
