5a29363220664b96172db2b411196755572daded
Bimsmart <bolaewenla66@gmail.com>
2023-10-02 10:07:14 -0700

    what is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - this is the main function
+ * @argc: argument to count
+ * @argv[]: a pointer to a string
+ * Return: 0 (Always)
+ */
+
 int main(int argc, char* argv[])
 {
 	int c = 0;
