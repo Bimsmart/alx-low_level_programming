1564506ed52007c4184fb63e85e2d5f9948158b4
Bimsmart <bolaewenla66@gmail.com>
2023-10-02 11:12:30 -0700

    printing the number of arguments passed to a program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,12 @@
 /**
  * main - This is the main function
  * @argc: argument to count
+ * @argv: argument to a pointer to a string
  * Return: 0
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
-	int c = 0;
-	printf("%d\n", argc);
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
