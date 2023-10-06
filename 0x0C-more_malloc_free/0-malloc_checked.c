39b353c1bce15488bd5a2d5ff4befd3348f6f066
Bimsmart <bolaewenla66@gmail.com>
2023-10-06 16:45:37 -0700

    this function exits normally if malloc fails

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 /**
  * malloc_checked - this function allocates memory using malloc
- * @b no of spaces to be allocated in memory
+ * @b no: of spaces to be allocated in memory
  * Return: void
  */
 void *malloc_checked(unsigned int b)
 {
 	void *c = malloc(b);
+
 	if (c == NULL)
-	{	
+	{
 		exit(98);
-	}	
+	}
 	return (c);
 }
