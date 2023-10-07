9727f69c38ddc4440c631172f09a29790886ab1c
Bimsmart <bolaewenla66@gmail.com>
2023-10-07 12:09:15 -0700

    this function concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,43 +5,42 @@
  * string_nconcat - this function concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: size 
+ * @n: size
  * Return: pointer or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ps;
-	unsigned int a, b;
+	unsigned int a = 0, b = 0, c = 0, d = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	a = b = 0;
-	n = a + b;
-	if (n >= b)
-		n = 	b;
-	a = b = 0;
-	while (s1[a] != '\0')
+	while (s1[a])
 		a++;
-	while (s2[b] != '\0')
+	while (s2[b])
 		b++;
-	ps = malloc(sizeof(char) * (n + 1));
+	if (n >= b)
+		c = a + b;
+	else
+		c = a + n;
+	ps = malloc(sizeof(char) * (c + 1));
 	if (ps == NULL)
 	{
 		return (NULL);
 	}
-	a = b = 0;
-	while( s1[a] != '\0')
+	b = 0;
+	while (d <= c)
 	{
-		ps[a] = s1[a];
-		a++;
+		if (d <= a)
+			ps[d] = s1[d];
+		if (d >= a)
+		{
+			ps[d] = s2[b];
+			b++;
+		}
+		d++;
 	}
-	while (s2[a] !=  '\0')
-	{
-		ps[a] = s2[b];
-		a++, b++;
-	}
-	ps[a] != '\0';
-	return(ps);
+	return (ps);
 }
