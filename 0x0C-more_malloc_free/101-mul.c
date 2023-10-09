5959207064e48ff7b6df8db429a7fd838bf4eb3b
Bimsmart <bolaewenla66@gmail.com>
2023-10-09 14:33:06 -0700

    this program multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,19 +4,20 @@
 /**
  * main - the main entry
  * @argv: argument to a pointer to a string @argc: argument to counter
+ * @argc: argument to count
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-        char *num1, *num2;
-        long int res;
+	char *num1, *num2;
+	long int res;
 
-        if (argc != 3)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-        else
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
 	{
 		num1 = argv[1];
 		num2 = argv[2];
