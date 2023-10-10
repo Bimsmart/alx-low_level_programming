433e881c9e6a977213589110d19c752290422886
Bimsmart <bolaewenla66@gmail.com>
2023-10-10 08:52:19 -0700

    struct dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - this is a structure
+ * @name: the name
+ * @age: how old
+ * @owner: who owns it
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
