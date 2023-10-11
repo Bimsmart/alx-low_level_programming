aec6fdb737385edb280cb8df6059e43a5c878a21
Bimsmart <bolaewenla66@gmail.com>
2023-10-11 08:46:23 -0700

    this function executes a function given as a prameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - this function exeecutes the action parameter
+ * @size: a struct
+ * @array: an array of elements
+ * @action: a function pointer to print integers
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	i = 0;
+	while (i <= size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
