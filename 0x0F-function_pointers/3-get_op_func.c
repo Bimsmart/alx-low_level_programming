a67ac1d23fe2b5f1e723c2744e4c591379266367
Bimsmart <bolaewenla66@gmail.com>
2023-10-11 15:53:51 -0700

    this program performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 /**
  * get_op_func - a pointer5 to get an operator input to a function
- * @s - a pointer to the available possible elements
+ * @s: a pointer to the available possible elements
  * Return: a pointer to the called function
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = 
+	op_t ops[] =
 	{
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    	};
-    	int i = 0;;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;;
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		i++;
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 /**
- * opp_add - this function adds two numbers
+ * op_add - this function adds two numbers
  * @a: first number
  * @b: second number
  * Return: a + b
@@ -11,7 +11,7 @@ int op_add(int a, int b)
 	return (a + b);
 }
 /**
- * opp_sub - this function adds two numbers
+ * op_sub - this function subtracts two numbers
  * @a: first number
  * @b: second number
  * Return: a - b
@@ -21,7 +21,7 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 /**
- * opp_mul - this function adds two numbers
+ * op_mul - this function multiplies two numbers
  * @a: first number
  * @b: second number
  * Return: a * b
@@ -31,17 +31,17 @@ int op_mul(int a, int b)
 	return (a * b);
 }
 /**
- * opp_div - this function adds two numbers
+ * op_div - this function dividess two numbers
  * @a: first number
  * @b: second number
  * Return: a / b
  */
-int op_div(int a,int b)
+int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
- * opp_mod - this function adds two numbers
+ * op_mod - this function calculates a remainder
  * @a: first number
  * @b: second number
  * Return: a % b
