94e5cf0a76a6cd5be9e4d2f611e9719d10dee8b2
Bimsmart <bolaewenla66@gmail.com>
2023-10-11 15:19:32 -0700

    this program performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * get_op_func - a pointer5 to get an operator input to a function
+ * @s - a pointer to the available possible elements
+ * Return: a pointer to the called function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = 
+	{
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    	};
+    	int i = 0;;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		return (NULL);
+	}	
+	return (ops[i].f);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * opp_add - this function adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * opp_sub - this function adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * opp_mul - this function adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * opp_div - this function adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+ */
+int op_div(int a,int b)
+{
+	return (a / b);
+}
+/**
+ * opp_mod - this function adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
