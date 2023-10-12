ddcfdd6cac5d74dcfd08470bffec6339ce13ea50
Bimsmart <bolaewenla66@gmail.com>
2023-10-12 06:17:27 -0700

    the function adds all its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - a function that sums up all its parameters
+ * @n : number of parameters
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int  sum = 0, c;
+
+	va_list(sum_total);
+	va_start(sum_total, n);
+	for (c = 0; c < n; c++)
+	{
+		sum = sum + va_arg(sum_total, int);
+	}
+	va_end(sum_total);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+This directory contains the following functions / programs
+0- a function that returns the sum of all its parameters.
+1- a function that prints numbers, followed by a new line.
+2- a function that prints strings, followed by a new line
+3- a function that prints anything
