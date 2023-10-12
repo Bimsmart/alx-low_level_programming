1cac533e36a599e6abab29afc4b0767f0c5df36f
Bimsmart <bolaewenla66@gmail.com>
2023-10-12 10:20:49 -0700

    this function prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,16 +9,17 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
+	unsigned int i = 0;	
+
 	va_list(numb);
 	va_start(numb, n);
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(numb, int));
-			if (i < n && i < n - 1)
+			if (i < (n - 1) && separator != NULL)
 			{
 				printf("%s", separator);
-			};
+			}
 		}
 	printf("\n");
 	va_end(numb);
