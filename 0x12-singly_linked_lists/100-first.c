925a07fe70dc0a11dba2e9533cd93130bdf3a421
Bimsmart <bolaewenla66@gmail.com>
2023-10-19 18:59:40 -0700

    this function prints before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * print_before_main - this function applies the constructor attribute 
+ * print_before_main - this function applies the constructor attribute
  * to the print_before main function before executing main
  */
-void print_before_main()
+void print_before_main(void)
 __attribute__((constructor));
 /**
  * print_before_main - whatvis to be printed
  */
-void print_before_main()
+void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
