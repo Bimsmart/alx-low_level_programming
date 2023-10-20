61b2aabc8082b0b2ee6f2ed1a84208ff715326d5
Bimsmart <bolaewenla66@gmail.com>
2023-10-20 14:51:30 -0700

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+message db 'Hello, Holberton', 0xA ;	The message to be printed
+section .text
+	global _start
+
+_start:
+	;Write the message to standard output
+	mov rax, 1	
+	mov rdi, 1	; System call number for write
+	mov rsi, message	; Address of the message
+	mov rdx, 16	; Length of the message
+	syscall
+
+	;
+	mov rax, 60	; system call number for exit
+	xor rdi, rdi	; Exit status 0
+	syscall
+
+
