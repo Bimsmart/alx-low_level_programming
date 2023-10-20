940f64cfc53485b5c39728dc6b11faec7d475f24
Bimsmart <bolaewenla66@gmail.com>
2023-10-19 18:19:20 -0700

    this function frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_list - this function frees a list
+ * @head: the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current_list = head;
+	list_t *next;
+
+	while (current_list != NULL)
+	{
+		next = current_list->next;
+		free(current_list);
+		current_list = next;
+	}
+}
