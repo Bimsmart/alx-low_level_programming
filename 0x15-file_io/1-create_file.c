98212cd58df9cae450c739a589f0965ebd8ead32
Bimsmart <bolaewenla66@gmail.com>
2023-10-30 16:01:52 -0700

    this function creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,10 +2,25 @@
 /**
  * create_file - this function  creates a file
  * @filename: name of the file to be created
- * @content: contents of the file to be created
+ * @text_content: contents of the file to be created
  * Return: 1 or -1
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	
+	int fd, nl, rwr;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = " ";
+	for (nl = 0; text_content[nl]; nl++)
+		;
+	rwr = write(fd, text_content, nl);
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
