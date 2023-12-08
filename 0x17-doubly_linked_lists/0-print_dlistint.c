9684c984fc86e8d6085b217147c08ad384e3ba1f
Bimsmart <bolaewenla66@gmail.com>
2023-12-08 15:54:55 -0800

    function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,8 @@
 /**
  *print_dlistint - this function prints all the elements of a dlistint_t
  *@h: a doubly linked list
- */
+ *Return: number of nodes
+ * /
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t no_of_nodes;
