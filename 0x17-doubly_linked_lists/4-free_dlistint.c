fdd400d237622ad9b083571f050df63c6ff39435
Bimsmart <bolaewenla66@gmail.com>
2023-12-08 17:18:59 -0800

    this function frees a double linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"    
+/**
+ * free_dlistint - this frees a double linked list
+ * @head: a pointer to the head
+ * Return: void
+ */
+void free_dlistint(dlistint_t* head)
+{
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
