acdff5358644d0fb2857c5730d0a514ecb53add4
Bimsmart <bolaewenla66@gmail.com>
2023-12-08 16:45:44 -0800

    adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *print_dlistint - this function prints all the elements of a dlistint_t
- *@h: a doubly linked list
- *Return: number of nodes
+ * print_dlistint - this function prints all the elements of a dlistint_t
+ * @h: a doubly linked list
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
